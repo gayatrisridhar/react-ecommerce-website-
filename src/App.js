7504c0e721b62e3d98e8d6bd4f7b9540eda6ae5a
gayatrisridhar <gayatrisridhar04@gmail.com>
2020-08-06 16:03:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Details from "./components/Details";
+import Productlist from "./components/Productlist";
+import Default from "./components/Default";
+import Cart from "./components/Cart/Cart";
+import { Switch, Route } from "react-router-dom";
+import Modal from "./components/Modal";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path="/" component={Productlist}></Route>
+        <Route path="/details" component={Details}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route component={Default}></Route>
+      </Switch>
+      <Modal></Modal>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Productlist.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { storeProducts } from "../data";
+import { ProductConsumer } from "../context";
+
+export default class Productlist extends Component {
+  state = {
+    products: storeProducts,
+  };
+  render() {
+    console.log(this.state.products);
+    return (
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products"></Title>
+            <div className="row">
+              <ProductConsumer>
+                {(value) => {
+                  return value.products.map((product) => {
+                    return (
+                      <Product key={product.id} product={product}></Product>
+                    );
+                  });
+                }}
+              </ProductConsumer>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+
+      //<Product>Productlist</Product>
+    );
+  }
+}
